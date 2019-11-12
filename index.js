01660cbf4fe336dc0a3203a03ac64cad753a79e0
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 20:13:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function createPost() {
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
 }
